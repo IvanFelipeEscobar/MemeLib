244d65e3fabee3a43d8135ddff1d7754c3bc3968
Ivan Escobar <ivnescbr1989@gmail.com>
2022-12-08 21:26:53 -0500

    added reddit meme fetch

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,26 @@
-var captionImgURL = `https://api.imgflip.com/get_memes`
+// var captionImgURL = `https://api.imgflip.com/get_memes`
+// $.ajax({
+//     url: captionImgURL,
+//     method: `GET`
+// }).then(function(response){
+//     console.log(response)
+//     response.data.memes.forEach(element => {
+//         console.log(element.name)
+//         var picEL = `<img src="${element.url}" id="${element.id}">`
+//         $(`.mainContent`).append(picEL)
+    
+//     })
+        
+//     });
+var captionImgURL = `https://www.reddit.com/r/memes.json`
 $.ajax({
     url: captionImgURL,
     method: `GET`
 }).then(function(response){
-    console.log(response)
-    response.data.memes.forEach(element => {
-        console.log(element.url)
-        var picEL = `<img src=${element.url}>`
+    console.log(response.data.children)
+    response.data.children.forEach(element => {
+        console.log(element.data)
+        var picEL = `<img src="${element.data.url_overridden_by_dest}">`
         $(`.mainContent`).append(picEL)
     
     })
