71270f2da9bcfb6e7924a719db5532d8fb5b6411
Ivan Escobar <ivnescbr1989@gmail.com>
2022-12-10 09:53:08 -0500

    js file edit v2

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,3 @@
-// var captionImgURL = `https://api.imgflip.com/get_memes`
-// $.ajax({
-//     url: captionImgURL,
-//     method: `GET`
-// }).then(function(response){
-//     console.log(response)
-//     response.data.memes.forEach(element => {
-//         console.log(element.name)
-//         var picEL = `<img src="${element.url}" id="${element.id}">`
-//         $(`.mainContent`).append(picEL)
-    
-//     })
-        
-//     });
-// $(`#submitForm`).on(`submit`, function(event){
-//     event.preventDefault()
-//     var memeAmount = $(`#memeAmount`).val()
-//     getMEME(memeAmount)
-// })
 var afterX = `` //starts after parameter as empty string which defaults to start
 var formSumbitEl = document.getElementById(`formSubmit`)
 var formSubmitHandler = function (event) {
@@ -28,31 +9,49 @@ var formSubmitHandler = function (event) {
      }
 function getMEME(memeAmount){
     
-var captionImgURL = `https://www.reddit.com/r/memes.json?limit=${memeAmount}&after=${afterX}`
-
+if($(`#memeType`).val()===`Blank Template`){
+    var captionImgURL = `https://api.imgflip.com/get_memes`
 $.ajax({
     url: captionImgURL,
     method: `GET`
 }).then(function(response){
+    console.log(response)
+    $(`#mainContent`).empty()
+    response.data.memes.forEach(element => {
+        console.log(element.name)
+        var picEL = ` 
+        <div class="card meme"> 
+            <div class="card-divider top">${element.name}
+            </div>
+            <img src="${element.url}">
+        </div>`
+        $(`.mainContent`).append(picEL)
+    })})
+    
 
-    
-    
+}else{
+
+var captionImgURL = `https://www.reddit.com/r/memes.json?limit=${memeAmount}&after=${afterX}`
+$.ajax({
+    url: captionImgURL,
+    method: `GET`
+}).then(function(response){ 
     afterX = response.data.after //getting after code will queue next available meme, next time button is clicked so that the same meme dont just pop up over and over
     $(`.mainContent`).empty()
     response.data.children.forEach(element => {
     //console.log(element.data)
     var picEL = ` 
-    <div class="card">
+    <div class="card meme"> 
         <div class="card-divider top">${element.data.title}
         </div>
         <img src="${element.data.url_overridden_by_dest}">
-    </div>`
+    </div>`//this html code will be rendered onto the html document in the main content area
     $(`.mainContent`).append(picEL)
         
        
     })
         
     });
-}
+}}
 
 formSumbitEl.addEventListener('submit', formSubmitHandler);
